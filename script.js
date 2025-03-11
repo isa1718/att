34fdfed63eeb04f0240c830d81f36b7a984f9c2e
isa1718 <isabelly.v.silva12@aluno.senai.br>
2025-03-11 14:35:20 -0300

    lçj

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function initTabNav{
+function initTabNav {
     const tabMenu = document.querySelectorAll('.js-tabmenu li');
     const tabContent = document.querySelectorAll('.js-tabcontent selection');
-    if(tabMenu.length && tabContent.length){
+    if(tabMenu.length && tabContent.length) {
         tabContent[0].classList.add('ativo');
         function activeTab(index) {
             tabContent.forEach((section) => {
@@ -10,11 +10,11 @@ function initTabNav{
             tabContent[index].classList.add('ativo');
         }
         tabMenu.forEach((itemMenu, index) => {
-            itemMenu.addEventListener('click') => {
+            itemMenu.addEventListener('click' () => {
                 activeTab(index);
-            })
+            });
 
-        }
+        });
     }
 }
 initTabNav();
