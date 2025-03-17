9908717c6218ac947ea5a4d953df918c204c8dd7
isa1718 <isabelly.v.silva12@aluno.senai.br>
2025-03-17 13:42:57 -0300

    dgx

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function initTabNav() {
+function iniTabNav() {
     const tabMenu = document.querySelectorAll('.js-tabmenu li');
-    const tabContent = document.querySelectorAll('.js-tabcontent section');
-  
-    if(tabMenu.length && tabContent.length) {
-      tabContent[0].classList.add('ativo');
-  
-      function activeTab(index) {
-        tabContent.forEach((section) => {
-          section.classList.remove('ativo');
+    const tabContent = document.querySelectorAll('.js-tabcontent section')
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo');
+
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
         });
-        tabContent[index].classList.add('ativo');
-      }
     }
-  }
-  initTabNav();
+}
+iniTabNav();
